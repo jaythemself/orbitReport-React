94ddaafd2b2d8931d9d99f66a7655d6b1a4c5998
Jay <138824448+jaythemself@users.noreply.github.com>
2023-08-13 16:14:13 -0500

    added banner function

---
+++ src/components/Banner.js
@@ -0,0 +1,7 @@
+function Banner {
+    //code block here
+    //return a <header> tag that says "Orbit Report"
+    //below header create <p> tag with instructions for users
+    //"Click on the buttons to see the satellites in that orbit type"
+    //export this function/import in Apps.js. nest the banner component in the App() function.
+}
