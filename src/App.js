3d03e4edacf0e1d4fa21851c90076deb6a51ff1b
Jay <138824448+jaythemself@users.noreply.github.com>
2023-08-13 16:59:26 -0500

    created task 3 vars in app.js and added properties to buttons and table

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
 import Buttons from "./components/Buttons";
 import Table from "./components/Table";
 import Banner from "./components/Banner";
+import {useState} from "react";
+import satData from "./components/satData";
 
 
 function App() {
+  //useState is passed satData, sat compares changes in state and setSat updates the state
+  const [sat, setSat] = useState(satData);
+  //uses Set method to create new array. used to create buttons, so pull out orbitType values
+  //Set prevents duplicate elements. If used map without set it would create 10 buttons instead of 3
+  const displaySats = [...new Set(satData.map((data) => data.orbitType))];
+  const filterByType = (currentType) => {
+    //update displaySats variable using filter method
+    //filter similar to map (iterate through collection until finds element)
+    //requires callback, newSatDisplay
+    const displaySats = satData.filter((newSatDisplay) => {
+      return newSatDisplay.orbitType === currentType;
+   });
+   setSat(displaySats);
+  };
   return (
     <div>
       <Banner />
-      <Buttons />
-      <Table />
+      <Buttons 
+        filterByType={filterByType}
+        setSat={setSat}
+        displaySats={displaySats}
+      />
+      <Table sat={sat} />
     </div>
   );
 }
