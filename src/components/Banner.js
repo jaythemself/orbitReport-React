9e7bedbb7b6c9c346269bee3d9888c69e43a86ae
Jay <138824448+jaythemself@users.noreply.github.com>
2023-08-13 16:37:01 -0500

    imported banner to app.js, fixed missed () in banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,7 +1,11 @@
-function Banner {
-    //code block here
-    //return a <header> tag that says "Orbit Report"
-    //below header create <p> tag with instructions for users
-    //"Click on the buttons to see the satellites in that orbit type"
+export default function Banner() {
     //export this function/import in Apps.js. nest the banner component in the App() function.
+    return (
+        <div>
+            <header>
+                <h1>Orbit Report</h1>
+            </header>
+            <p>Click on the buttons to see the satellites in that orbit type</p>
+        </div>
+    )
 }
